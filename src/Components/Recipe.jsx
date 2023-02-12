c7ba135a7f68e4b652e19106edf7f19263def318
DubeyRitik <ritikdubeysuraj@gmail.com>
2023-02-12 22:29:52 +0530

    added all the cards and connected to api

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,10 +1,69 @@
-import React from "react";
-
+import { useEffect, useState, useRef } from "react";
+import RecipeCard from "./RecipeCard";
+import styles from "../Recipe.module.css";
 
 export default function Recipe() {
-    return (
-      <div>
-       
-      </div>
-    );
+  const [typeMain, setTypeMain] = useState(true);
+  const [type2, setType2] = useState(false);
+  const inputRef = useRef(null);
+  function onSearch() {
+    setType2(true);
+    setTypeMain(false);
+    let searchItem = inputRef.current.value;
+    getSearched(searchItem);
   }
+  const [popular, setPopular] = useState([]);
+  const [searched, setSearched] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=279925372bab43d28538c44a31f0a3e5&number=50`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+    console.log(data);
+  };
+
+  const getSearched = async (searchItem) => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=279925372bab43d28538c44a31f0a3e5&query=${searchItem}&number=50`
+    );
+    const data = await api.json();
+    setSearched(data.results);
+    console.log(data.results);
+    console.log("searched clicked");
+  };
+
+  return (
+    <div>
+      <label htmlFor="">Find the recipes</label>
+      <input type="text" ref={inputRef} />
+      <button onClick={onSearch}>Search</button>
+      <div className={styles.cardDiv}>
+        {type2 &&
+          searched.map((recipe) => {
+            return (
+              <RecipeCard
+                title={recipe.title}
+                id={recipe.id}
+                image={recipe.image}
+              ></RecipeCard>
+            );
+          })}
+        {typeMain &&
+          popular.map((recipe) => {
+            return (
+              <RecipeCard
+                title={recipe.title}
+                id={recipe.id}
+                image={recipe.image}
+              ></RecipeCard>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -1,9 +1,26 @@
+import styles from "../RecipeCard.module.css";
+
 function RecipeCard(props) {
+  const id = props.id;
+
+  async function findUrl() {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=279925372bab43d28538c44a31f0a3e5&includeNutrition=false`
+    );
+    const data = await api.json();
+    let url = data.sourceUrl;
+    console.log("this url found");
+    console.log(data);
+    window.open(url, "_blank");
+  }
+
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <h1>hello</h1>
-    </div>
+    <button target="_blank" rel="noopener noreferrer" onClick={findUrl}>
+      <div className={styles.card}>
+        <img src={props.image} alt="" />
+        <h1>{props.title}</h1>
+      </div>
+    </button>
   );
 }
 
